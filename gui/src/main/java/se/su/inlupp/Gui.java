d0d25f2b31972e7a53e78b0f992538971aced264
MarcAngelo Ferri <Marckangelo808@gmail.com>
2025-07-10 18:06:06 +0200

    new map

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -6,13 +6,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
 
 import java.io.*;
 import java.util.HashMap;
@@ -20,21 +17,30 @@ import java.util.Map;
 
 
 public class Gui extends Application {
+    private Graph<String> graph = new ListGraph<String>();
     private Stage stage;
     private FileChooser fileChooser = new FileChooser();
     private Map<String, String> maps = new HashMap<>();
     private final ImageView imageView = new ImageView();
+    private Pane center;
+    private String from;
+    private String to;
+    private boolean changed;
+
 
     public void start(Stage primaryStage) {
         stage = primaryStage;
-        Graph<String> graph = new ListGraph<String>();
         primaryStage.setTitle("PathFinder");
         Image icon = new Image("icon.png");
         primaryStage.getIcons().add(icon);
         BorderPane root = new BorderPane();
         root.setStyle("-fx-background-color: #2E2E2E;");
+
         VBox vbox = new VBox();
         root.setTop(vbox);
+
+         center = new Pane();
+         root.setCenter(center);
 
         MenuBar menuBar = createMenuBar();
         vbox.getChildren().add(menuBar);
@@ -46,6 +52,8 @@ public class Gui extends Application {
 
         root.setCenter(imageView);
         Scene scene = new Scene(root, 1000, 800);
+
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -55,11 +63,11 @@ public class Gui extends Application {
         topButtons.setAlignment(Pos.BOTTOM_CENTER);
         topButtons.setHgap(5);
 
-        Button findButton =  styleButtons("Find Path");
-        Button showButton =  styleButtons("Show Connection");
-        Button newPlaceButton =  styleButtons("New Place");
-        Button newConnectionButton =  styleButtons("New Connection");
-        Button changeConnectionButton =  styleButtons("Change Connection");
+        Button findButton = styleButtons("Find Path");
+        Button showButton = styleButtons("Show Connection");
+        Button newPlaceButton = styleButtons("New Place");
+        Button newConnectionButton = styleButtons("New Connection");
+        Button changeConnectionButton = styleButtons("Change Connection");
 
         topButtons.getChildren().add(findButton);
         topButtons.getChildren().add(showButton);
@@ -69,7 +77,9 @@ public class Gui extends Application {
 
         return topButtons;
     }
-    private Button styleButtons(String text){
+
+    //Ändrar färg på ikoner och när man hovrar
+    private Button styleButtons(String text) {
         Button button = new Button(text);
         // Ursprunglig stil
         String baseStyle = "-fx-background-color: #555879;" +
@@ -96,7 +106,6 @@ public class Gui extends Application {
         return button;
     }
 
-
     private MenuBar createMenuBar() {
         Menu menu = new Menu("File");
 
@@ -120,63 +129,78 @@ public class Gui extends Application {
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(menu);
 
+        //Lägger till färg på menuBar
+        menuBar.setStyle(
+                "-fx-text-fill: white;" +
+                        "-fx-font-family: 'Segoe UI';" +
+                        "-fx-font-weight: bold;"
+        );
+
         return menuBar;
     }
 
-  private void handleOpen() {
-    fileChooser.getExtensionFilters().clear();
-    fileChooser.getExtensionFilters().add(
-            new FileChooser.ExtensionFilter("Graph Files","Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
-    );
-    fileChooser.setTitle("Välj graf-fil");
-
-    File file = fileChooser.showOpenDialog(stage);
-    if (file != null) {
-      loadGraphFile(file);
+    private void loadGraphFile(File file) {
+        // Här implementerar du laddning av graf-fil senare
+        // För nu bara testa att fildialogen fungerar
     }
-  }
 
-  private void loadGraphFile(File file) {
-    System.out.println("Graf-fil vald: " + file.getName());
-    // Här implementerar du laddning av graf-fil senare
-    // För nu bara testa att fildialogen fungerar
-  }
+    private void handleOpen() {
+        fileChooser.getExtensionFilters().clear();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Graph Files", "*.graph")
+        );
+        fileChooser.setTitle("Open Graf file");
 
-  private void handleNewMap() {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            loadGraphFile(file);
+        }
+    }
+
+    private void handleNewMap() {
         fileChooser.getExtensionFilters().clear();
         fileChooser.getExtensionFilters().add(
                 new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"));
 
-      //Detta anväder vi för att öppna resource mapen
-        File startFolder = new File((System.getProperty("user.home")), "Pictures");
-      if (startFolder.exists()) {
-          fileChooser.setInitialDirectory(startFolder);
-      }
+        //Detta anväder vi för att öppna resource mapen (Funger dock bara på denna dator)
+        File startFolder = new File("C:\\Users\\marck\\Desktop\\DSV matrial\\Prog2\\Inlämningsuppgiften\\gui\\src\\main\\resources");
+        if (startFolder.exists()) {
+            fileChooser.setInitialDirectory(startFolder);
+        }
 
-        fileChooser.setTitle("Select map");
+        fileChooser.setTitle("Select new map");
         File file = fileChooser.showOpenDialog(stage);
-        if(file != null){
-          loadImageFile(file);
+        if (file != null) {
+            loadImageFile(file);
         }
 
     }
+        //SNYGGA TILL!!!
+    private void loadImageFile(File file) {
+        try {
+            Image image = new Image(file.toURI().toString());
+            imageView.setImage(image);
 
-  private void loadImageFile(File file) {
-    try {
-      System.out.println("Försöker ladda bild: " + file.getAbsolutePath());
+            center.getChildren().retainAll();
 
-      Image image = new Image(file.toURI().toString());
-      imageView.setImage(image);
-      imageView.setPreserveRatio(true);
-      imageView.setFitWidth(700);
+            graph = new ListGraph<>();
+            from = null;
+            to = null;
 
-    } catch (Exception e) {
-      System.err.println("Error when trying to load image: " + e.getMessage());
-      e.printStackTrace();
+            stage.setHeight(image.getHeight()+100);
+            stage.setWidth(image.getWidth()+100);
+            stage.centerOnScreen();
+
+            changed = true;
+
+
+        } catch (Exception e) {
+            System.err.println("Error when trying to load image: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 }
